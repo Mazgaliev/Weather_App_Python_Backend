a285d959e9aec91ca5f8f10f9192cf8c49ca6856
Mite <mite_mazgaliev@live.com>
2024-08-15 20:08:41 +0200

    Bulk station measurement requests

--- Weather_App_Python_Backend/urls.py
+++ Weather_App_Python_Backend/urls.py
@@ -31,35 +31,47 @@ url = os.getenv("OPENWEATHERMAP_API_URL")
 @csrf_exempt   
 def scrape_data(request):
 
-    latitude = request.POST.get("latitude")
-    longitude = request.POST.get("longitude")
+    # latitude = request.POST.get("latitude")
+    # longitude = request.POST.get("longitude")
+    # station_id = request.POST.get('stationId')
+    payload = json.loads(request.body)
+    stations = payload.get("stations_payload")
 
+    if stations== None:
+        return JsonResponse({'Result':[], 'Error':True, 'Message':f'EMPTY stations payload'})
+        
     # Simple logic to scrape data
     now = datetime.now()
     start = int((now - timedelta(hours=5)).timestamp())
     end = int(now.timestamp())
 
-    params = {
-        'lat':latitude,
-        'lon':longitude,
-        'start':start,
-        'end':end,
-        'type':'hour',
-        'appid':api_key}
-    req = requests.models.PreparedRequest()
-    req.prepare_url(url, params)
-    
-    response = requests.get(req.url)
-    print(req.url)
+    final_result = []
 
-    print(response)
-    data = _parse_data(response)
+    for station in stations:
+        longitude = station.get("Longitude") 
+        latitude = station.get("Latitude")
+        station_id = station.get("Id")
+        params = {
+            'lat':latitude,
+            'lon':longitude,
+            'start':start,
+            'end':end,
+            'type':'hour',
+            'appid':api_key}
+        
+        req = requests.models.PreparedRequest()
+        req.prepare_url(url, params)
+        
+        response = requests.get(req.url)
 
-    return JsonResponse(data)
+        data = _parse_data(response, station_id)
+        final_result.append(data)
+    return JsonResponse({"Result" : final_result})
 
 @csrf_exempt   
 def train_models(request):
     # Simple logic to train models
+    all_data = requests.POST.get('data')
     model_status = "Model trained successfully"
     return JsonResponse({'status': 'success', 'message': model_status})
 
@@ -69,7 +81,7 @@ def predict_values(request):
     predictions = {"result": "some predicted values"}
     return JsonResponse({'status': 'success', 'predictions': predictions})
 
-def _parse_data(response):
+def _parse_data(response, station_id):
     final_results = []
     if response.status_code == 200:
         data = json.loads(response.content)
@@ -93,6 +105,10 @@ def _parse_data(response):
                 parsed_result['pm2_5'] = components.get('pm2_5')
                 parsed_result['pm10'] = components.get('pm10')
                 parsed_result['aqi'] = aqi
+                parsed_result['so2'] = components.get('so2')
+                parsed_result['stationId'] = int(station_id)
+                parsed_result['measurementTime'] = result['dt']
+
                 final_results.append(parsed_result)
     else:
         return {'Result':[], 'Error':True, 'Message':f'Problem with response from OpenweatherAPI {response.content}'}
